6bcef0ac2f89179bca3d098bd1955c664412b572
Thomas Eggerling <thomas.eggerling@posteo.de>
2024-02-09 10:23:23 +0100

    Design UI

--- CsvViewer/Program.cs
+++ CsvViewer/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CsvViewer;
+using Material.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient
 		{BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddMBServices();
 
 await builder.Build().RunAsync();
